b62712ee037f7cfbb8ac6f6644bf221939e6d854
SokolovDS <sokolov.exe@gmail.com>
2020-01-02 00:00:41 +0500

    Add auth pages - #6

--- services/models.py
+++ services/models.py
@@ -1,5 +1,6 @@
 from django.db.models import Model, CharField, EmailField, TextField, DecimalField
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -11,6 +12,9 @@ class Service(Model):
     name = CharField(max_length=64)
     description = TextField(blank=True, null=True, default=None)
     min_price = DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def get_absolute_url(self):
+        return reverse('service_detail', args=[str(self.id)])
 
     def __str__(self):
         return "Name: {}, Price: {}".format(self.name, self.min_price)
--- services/views.py
+++ services/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from services.models import Service
+from django.views import generic
 
 
 # Create your views here.
 
-def service_list(request):
-    service_list = Service.objects.filter()
-    return render(request, 'services/service_list.html', locals())
+
+class ServiceListView(generic.ListView):
+    model = Service
+
+
+class ServiceDetailView(generic.DetailView):
+    model = Service
+
+# def service_list(request):
+#     service_list = Service.objects.filter()
+#     return render(request, 'services/service_list.html', locals())

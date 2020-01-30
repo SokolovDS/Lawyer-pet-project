e79050907a455f600ee584dd7391b0aa8f0db8e5
SokolovDS <sokolov.exe@gmail.com>
2020-01-31 00:24:13 +0300

    #9 add lawyers

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'main',
     'services',
     'events',
+    'lawyer'
 ]
 
 MIDDLEWARE = [
--- lawyer/urls.py
+++ lawyer/urls.py
@@ -13,16 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-
-admin.autodiscover()
+from django.urls import path
+from lawyer import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('services/', include('services.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('lawyers/', include('lawyer.urls')),
-
+    path('', views.LawyerListView.as_view(), name='lawyer'),
+    path('<int:pk>/', views.LawyerDetailView.as_view(), name='lawyer_detail'),
 ]
